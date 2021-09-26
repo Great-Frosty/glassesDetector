c2d669a6af3a2ddf6faecff82e171877431e22ea
Great Frosty <pasha.kochetov.work@gmail.com>
2021-09-26 12:43:20 +0300

    Detection template

--- detect_glasses.py
+++ detect_glasses.py
@@ -7,6 +7,7 @@ def install():
 install()
 print('\n---Additional libraries installation complete---')
 
+import time
 import os
 import cv2
 # from collections import defaultdict
@@ -17,10 +18,14 @@ from torch.utils.data import Dataset, DataLoader
 import torch.optim
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
+from facenet_pytorch import MTCNN
+import numpy as np
 
 
+start_time = time.time()
 img_dir = sys.argv[-1]
 
+mtcnn = MTCNN(128, margin=8)
 class GlassesDataset(Dataset):
     def __init__(self, images_filepaths, transform=None):
         self.images_filepaths = images_filepaths
@@ -33,29 +38,46 @@ class GlassesDataset(Dataset):
         image_filepath = self.images_filepaths[idx]
         image = cv2.imread(os.path.join(img_dir, image_filepath))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
+        image = mtcnn(image)
+        image = torch.swapaxes(image, 0, 2)
         if self.transform is not None:
+            image = np.array(image)
             image = self.transform(image=image)["image"]
         return image
 
 
-class BaselineModel(nn.Module):
+class DetectionModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv_1 = nn.Conv2d(3, 32, 3, padding=1, stride=2) # stride 2 for homemade maxpool
-        self.lin_1 = nn.Linear(64**2*32, 1)
-        self.relu = nn.LeakyReLU(0.15)
 
+        self.layer = nn.Sequential(
+            nn.Conv2d(3, 32, 5, stride=1, padding=2),
+            nn.LeakyReLU(0.1),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 32, 3, padding=1, stride=1),
+            nn.LeakyReLU(0.1),
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(32, 64, 5, stride=1, padding=2),
+            nn.LeakyReLU(0.1),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 128, 3, padding=1, stride=1),
+            nn.LeakyReLU(0.1),
+            nn.MaxPool2d(2, stride=2)
+        )
+
+        self.decoder = nn.Sequential(
+            # nn.Dropout2d(p=0.3),
+            nn.Linear(32**2*128, 1)
+        )
 
     def forward(self, images):
-        x = self.conv_1(images)
-        x = self.relu(x)
-        x = x.view(-1, 64**2*32)
-        x = self.lin_1(x)
+        x = self.layer(images)
+        x = x.view(-1, 32**2*128)
+        x = self.decoder(x)
         return x
 
 
-model = BaselineModel()
+model = DetectionModel()
 model_path = os.path.join(os.getcwd(), 'trained_model/model_weights.pt')
 model = model.eval()
 model.load_state_dict(torch.load(model_path))
@@ -65,8 +87,10 @@ trained_std = (60.459537506104, 53.603122711182, 53.296249389648)
 images_paths = os.listdir(img_dir)
 target = torch.Tensor([0 if ')' in filename else 1 for filename in images_paths])
 
+
+# заменить на torchvsion - меньше импортов
+# перейти с ргб на grayscale
 eval_transform = A.Compose([
-                    A.Resize(128, 128),
                     A.Normalize(mean=trained_mean, std=trained_std),
                     ToTensorV2(),
 ])
@@ -90,9 +114,10 @@ with torch.no_grad():
         output = model(images)
         predictions = (torch.sigmoid(output) >= 0.5)[:, 0]
         predicted_paths += [images_paths[i] for i, gls_detected in enumerate(predictions) if gls_detected]
-    print(f'\nAccuracy = {calculate_accuracy(output, target):.2%}')
+    print(f'\n---Accuracy = {calculate_accuracy(output, target):.2%}---\n')
 
 [print(p) for p in predicted_paths]
+print(f'\n---Inference time: {(time.time() - start_time) / len(images_paths) * 1000:.2f} milliseconds per image ---\n')
 
 
 
