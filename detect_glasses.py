62cec89d3187af0202df9130d9de7e0f4e0b2e47
Great Frosty <pasha.kochetov.work@gmail.com>
2021-09-30 00:31:54 +0300

    Detection functions at 80%

--- detect_glasses.py
+++ detect_glasses.py
@@ -38,51 +38,72 @@ class GlassesDataset(Dataset):
         image_filepath = self.images_filepaths[idx]
         image = cv2.imread(os.path.join(img_dir, image_filepath))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = mtcnn(image)
-        image = torch.swapaxes(image, 0, 2)
+        # image = torch.swapaxes(image, 0, 2)
         if self.transform is not None:
             image = np.array(image)
             image = self.transform(image=image)["image"]
         return image
 
 
-class DetectionModel(nn.Module):
-    def __init__(self):
+class ConvModel(nn.Module):
+    def __init__(self) -> None:
         super().__init__()
 
         self.layer = nn.Sequential(
-            nn.Conv2d(3, 32, 5, stride=1, padding=2),
-            nn.LeakyReLU(0.1),
+            nn.Conv2d(3, 32, 3, stride=1, padding=1),
             nn.BatchNorm2d(32),
-            nn.Conv2d(32, 32, 3, padding=1, stride=1),
+            nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2), # 112*112
+            nn.Conv2d(32, 32, 3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.LeakyReLU(0.1),
-            nn.MaxPool2d(2, stride=2),
-            nn.Conv2d(32, 64, 5, stride=1, padding=2),
+            nn.Conv2d(32, 32, 3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.LeakyReLU(0.1),
+            nn.MaxPool2d(2), # 66 * 66
+            nn.Conv2d(32, 64, 3, stride=1, padding=1),
             nn.BatchNorm2d(64),
-            nn.Conv2d(64, 128, 3, padding=1, stride=1),
             nn.LeakyReLU(0.1),
-            nn.MaxPool2d(2, stride=2)
+            nn.MaxPool2d(2), # 33 * 33
+            nn.Conv2d(64, 64, 3, padding=1, stride=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.05),
+            nn.Conv2d(64,128,3, padding=1, stride=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.05),
+            nn.MaxPool2d(2), # 16 * 16
+            nn.Conv2d(128, 128, 3, padding=1, stride=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.05),
+            nn.MaxPool2d(2), # 8*8
+            nn.Conv2d(128, 256, 3, padding=1, stride=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.02),
+            nn.Conv2d(256, 256, 3, padding=1, stride=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.02),
+            nn.MaxPool2d(2), # 4*4
+            nn.Conv2d(256,512, 3, padding=1, stride=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.01),
+            nn.AdaptiveMaxPool2d(2), # 2*2
         )
-
-        self.decoder = nn.Sequential(
-            # nn.Dropout2d(p=0.3),
-            nn.Linear(32**2*128, 1)
+        self.classifier = nn.Sequential(
+            # nn.Dropout(p=0.2),
+            nn.Linear(4*512, 1),
         )
 
     def forward(self, images):
         x = self.layer(images)
-        x = x.view(-1, 32**2*128)
-        x = self.decoder(x)
+        x = x.view(-1, 4*512)
+        x = self.classifier(x)
         return x
 
 
-model = DetectionModel()
-model_path = os.path.join(os.getcwd(), 'trained_model/model_weights.pt')
+model = ConvModel()
+model_path = os.path.join(os.getcwd(), 'trained_model/conv_net_weights.pt')
 model = model.eval()
 model.load_state_dict(torch.load(model_path))
-trained_mean = (136.352539062500, 112.022689819336,  96.958969116211)
-trained_std = (60.459537506104, 53.603122711182, 53.296249389648)
 
 images_paths = os.listdir(img_dir)
 target = torch.Tensor([0 if ')' in filename else 1 for filename in images_paths])
@@ -91,7 +112,9 @@ target = torch.Tensor([0 if ')' in filename else 1 for filename in images_paths]
 # заменить на torchvsion - меньше импортов
 # перейти с ргб на grayscale
 eval_transform = A.Compose([
-                    A.Normalize(mean=trained_mean, std=trained_std),
+                    A.SmallestMaxSize(max_size=230),
+                    A.CenterCrop(height=224, width=224),
+                    A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
                     ToTensorV2(),
 ])
 
@@ -103,7 +126,7 @@ def calculate_accuracy(predictions, target):
     target = target.view(-1, 1) == 1
     return torch.true_divide((target == predictions).sum(dim=0), output.size(0)).item()
 
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 device = torch.device('cpu')
 
 predicted_paths = []
