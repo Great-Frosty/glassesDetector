23dcba7c28e7f72cdce6654b11753bddfb392c62
Great Frosty <pasha.kochetov.work@gmail.com>
2021-10-03 15:25:45 +0300

    kinda working version

--- detect_glasses.py
+++ detect_glasses.py
@@ -10,7 +10,6 @@ print('\n---Additional libraries installation complete---')
 import time
 import os
 import cv2
-# from collections import defaultdict
 
 import torch
 import torch.nn as nn
@@ -19,8 +18,9 @@ import torch.optim
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 import numpy as np
-import torchvision.models as models
+from torchvision import models
 
+from facenet_pytorch import MTCNN
 
 img_dir = sys.argv[-1]
 
@@ -36,66 +36,28 @@ class GlassesDataset(Dataset):
         image_filepath = self.images_filepaths[idx]
         image = cv2.imread(os.path.join(img_dir, image_filepath))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # image = torch.swapaxes(image, 0, 2)
         if self.transform is not None:
             image = np.array(image)
             image = self.transform(image=image)["image"]
         return image
 
 
-# class ConvModel2(nn.Module):
-#     def __init__(self) -> None:
-#         super().__init__()
-
-    #     self.layer = nn.Sequential(
-    #         nn.Conv2d(3,12,3, padding=1, stride=1),
-    #         nn.BatchNorm2d(12),
-    #         nn.ReLU(),
-    #         nn.MaxPool2d(4, stride=4), # 66
-    #         nn.Conv2d(12,24,3, padding=1, stride=1),
-    #         nn.BatchNorm2d(24),
-    #         nn.ReLU(),
-    #         nn.MaxPool2d(4, stride=4), # 33
-    #         nn.Conv2d(24,48,3, padding=1, stride=1),
-    #         nn.BatchNorm2d(48),
-    #         nn.ReLU(),
-    #         nn.MaxPool2d(2), # 16
-    #         nn.Conv2d(48,96,3, padding=1, stride=1),
-    #         nn.BatchNorm2d(96),
-    #         nn.ReLU(),
-    #         nn.AdaptiveMaxPool2d(4),
-    #     )
-
-    #     self.classifier = nn.Sequential(
-    #         nn.Linear(4**2*96, 1)
-    #     )
-
-    # def forward(self, images):
-    #     x = self.layer(images)
-    #     x = x.view(-1, 4**2*96)
-    #     x = self.classifier(x)
-    #     return x
-
-
-model = getattr(models, 'mobilenet_v3_small')(pretrained=False)
+model = getattr(models, 'mobilenet_v3_small')(pretrained=True,)
 model.classifier = nn.Sequential(
-    nn.Linear(in_features=576, out_features=1024, bias=True),
-    nn.Hardswish(),
     nn.Dropout(p=0.2, inplace=True),
-    nn.Linear(in_features=1024, out_features=1, bias=True),
+    nn.Linear(in_features=48, out_features=1, bias=True),
+
 )
-model_path = os.path.join(os.getcwd(), 'trained_model/transfer_weights.pt')
-model = model.eval()
+model.features = nn.Sequential(*list(model.children())[-3][:8])
+
+model_path = os.path.join(os.getcwd(), 'trained_model/mobnet_weights.pt')
 model.load_state_dict(torch.load(model_path))
+model = model.eval()
 
 images_paths = os.listdir(img_dir)
 target = torch.Tensor([0 if ')' in filename else 1 for filename in images_paths])
-
-
-# заменить на torchvsion - меньше импортов
-# перейти с ргб на grayscale
 eval_transform = A.Compose([
-                    A.SmallestMaxSize(max_size=230),
+                    A.SmallestMaxSize(max_size=300),
                     A.CenterCrop(height=224, width=224),
                     A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
                     ToTensorV2(),
@@ -125,8 +87,9 @@ with torch.no_grad():
         predicted_paths += [images_paths[i] for i, gls_detected in enumerate(predictions) if gls_detected]
     print(f'\n---Accuracy = {calculate_accuracy(output, target):.2%}---\n')
 
-[print(p) for p in predicted_paths]
-print(f'\n---Inference time: {(time.time() - start_time) / len(images_paths) * 1000:.2f} milliseconds per image ---\n')
+inf_time = time.time() - start_time
+[print(path) for path in predicted_paths]
+print(f'\n---Inference time: {(inf_time) / len(images_paths) * 1000:.2f} milliseconds per image ---\n')
 
 
 
